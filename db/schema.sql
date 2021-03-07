22c110338a4e12526eb3a0dc06c9ab9bf8b9ec5a
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-03-07 00:07:42 -0800

    add employee info

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS employeeDB;
-CREATE DATABASE employeeDB;
-USE employeeDB;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
 CREATE TABLE employee (
     id INTEGER auto_increment PRIMARY KEY NOT NULL,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
-    role_id INTEGER,
-    manager_id INTEGER NULL
+    role_id INTEGER UNSIGNED,
+    manager_id INTEGER UNSIGNED NULL
 );
 
 CREATE TABLE department (
@@ -19,5 +19,5 @@ CREATE TABLE role (
     id INTEGER auto_increment PRIMARY KEY NOT NULL,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL(9, 2) NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER UNSIGNED NOT NULL
 );
---
+++ db/seed.sql
@@ -0,0 +1,21 @@
+USE employee_db;
+
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Angela', 'Gates', 2, NULL),
+    ('Bonnie', 'Perkins', 1, NULL),
+    ('Frank', 'DELLING', 3, 1),
+    ('Joe', 'GEONI', 2, 2),
+    ('Kimberly', 'Bissot', 4, NULL),
+    ('Lisa', 'Landry', 1, 1),
+    ('Michael', 'Markle', 6, NULL),
+    ('Patrick', 'Mourgos', 7, 2),
+    ('Rose', 'King', 5, NULL),
+    ('Todd', 'Hunold', 5, 1);
+
+
+
+
+
+
+
