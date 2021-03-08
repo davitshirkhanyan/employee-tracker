3676a330d51e4036e442e9d59db7155b2de572d5
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-03-07 16:27:58 -0800

    create Database class

--- server.js
+++ server.js
@@ -2,6 +2,43 @@ const mysql = require('mysql');
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+// create Database class
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(rows);
+            });
+        });
+    };
+
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    };
+};
+
+const db = new Database({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Dav1234',
+    databas: 'employee_db'
+});
+
 // add function to start the app
 const startServer = () => {
     inquirer.prompt({
